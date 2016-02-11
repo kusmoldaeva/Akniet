e678d7d835b4e3a3d86bd42ff1e764246dba94fa
akniet <aknietkusmoldaeva@gmail.com>
2016-02-11 11:44:46 +0600

    commited all lab works

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,11 +6,38 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+    class Complex
     {
-        static void Main(string[] args)
+        public int a, b;
+        public Complex(int a, int b)
         {
+            this.a = a;
+            this.b = b;
 
         }
-    }
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex c3 = new Complex(c1.a + c2.a, c1.b + c2.b);
+            return c3;
+        }
+        public override string ToString()
+            {
+                return a + " " + "and" + " " + b;
+
+            }
+
+        }
+        class program
+        {
+            static void Main(String[] args)
+            {
+                Complex First = new Complex(12, 25);
+                Complex Second = new Complex(26, 85);
+                Complex answer = First + Second;
+                Console.WriteLine(answer);
+                Console.ReadKey();
+
+            }
+        }
+    
 }
